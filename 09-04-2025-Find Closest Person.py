890c421ef6c86f84445814aa421fbf60d197fe3e
Ilyes HAMDI <ilyesilyeshamdi10@gmail.com>
2025-09-04 18:40:56 +0100

    leetcode

---
+++ 09-04-2025-Find Closest Person.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/find-closest-person/description/
+# Time:O(1)
+# 
+# # Space:O(n)
+# Python:O(1)
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        if abs(x-z)==abs(y-z):
+            return 0
+        elif abs(x-z)<abs(y-z):
+            return 1
+        else:
+            return 2
