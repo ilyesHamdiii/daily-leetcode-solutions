47174322947da7f4e2243bbf0f0e24a5f51fd85e
Ilyes HAMDI <ilyesilyeshamdi10@gmail.com>
2025-10-15 15:06:51 +0100

    leetcode

---
+++ 10-15-2025-Adjacent Increasing Subarrays Detection II.py
@@ -0,0 +1,35 @@
+#https://leetcode.com/problems/adjacent-increasing-subarrays-detection-ii/description/
+# Time:O(n)
+# Medium
+# Space:O(n)
+
+
+class Solution:
+    def maxIncreasingSubarrays(self, nums: List[int]) -> int:
+        INF = 10 ** 20
+
+        best = 0
+        streaks = []
+        count = 0
+        last = -INF
+
+        for x in nums:
+            if x > last:
+                count += 1
+            else:
+                count = 1
+            
+            last = x
+            streaks.append(count)
+        
+        best = max(streaks) // 2
+
+        for i in range(len(streaks)):
+            if streaks[i] <= best:
+                continue
+            if i - streaks[i] < 0:
+                continue
+            
+            if streaks[i - streaks[i]] >= streaks[i]:
+                best = max(best, streaks[i])
+        return best
